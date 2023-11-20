121ab1a4d84e201f3efb4853bb91077fce5d0c2d
KoroEcem <ecemkoro@gmail.com>
2023-11-20 15:01:46 +0300

    add if else homework

--- src/main/java/com/ecemkoro/kararKontrolMekanizmalari/IfElse.java
+++ src/main/java/com/ecemkoro/kararKontrolMekanizmalari/IfElse.java
@@ -6,6 +6,7 @@ public class IfElse {
   public static void main(String[] args) {
     System.out.println("IF ELSE");
 
+    /*
     if (1<50){
       System.out.println("EVET UYGUNDUR");
     } else {
@@ -41,26 +42,84 @@ public class IfElse {
 
     //TODO: ÖDEV: Bu kodu sağlıklı ise yaş kontrolüne girecek şekilde düzelt. Sağlıksızsa kontrole sokma!
     System.out.println("-------EHLİYET KULLANMA 1.Durum-----------");
-    int adayKisininYasi = 18;
-    boolean saglikDurumu = true;
+    int adayKisininYasi = 25;
+    boolean saglikDurumu = false;
 
-    if (adayKisininYasi == 16){
+    if (adayKisininYasi == 16 ){
+
       if (saglikDurumu == true){
-      System.out.println("Aday stajyer olabilir.");
-      System.out.println("Sağlıklı");
-      } else {
-        System.out.println("Sağlıklı değil");
-      }
-    } else if (adayKisininYasi > 17) {
-      if (saglikDurumu == false) {
-        System.out.println("Aday reşit, ehliyet alabilir.");
+        System.out.println("Aday stajyer olabilir.");
         System.out.println("Sağlıklı");
       } else {
-        System.out.println("Sağlıklı değil");
+        System.out.println("Sağlıklı değil.");
+      }
+
+    } else if (adayKisininYasi > 17 ) {
+
+      if (saglikDurumu == true){
+        System.out.println("Reşit ehliyet alabilir.");
+        System.out.println("Sağlıklı");
+
+      } else {
+        System.out.println("Sağlıklı değil.");
+      }
+
+    } else  {
+      System.out.println("Üzgünüm ehliyet için uygun değilsiniz.");
+    }
+     */
+
+
+    System.out.println("-------EHLİYET KULLANMA ÖDEV ÇÖZÜMÜ-----------");
+
+    //Kullanıcı girdisini okumak için bir Scanner nesnesi oluşturulur. System.in konsoldan girdi almak için kullanılır.
+    Scanner scanner = new Scanner(System.in);
+
+    // Kullanıcıdan yaş bilgisi alınıyor
+    int adayinYasi;
+    while (true) {
+      System.out.print("Lütfen adayın yaşını giriniz: ");
+      if (scanner.hasNextInt()) {   // if (scanner.hasNextInt()) kontrolü, girilen değerin bir tamsayı (int) olup olmadığını kontrol eder.
+        adayinYasi = scanner.nextInt(); // adayinYasi = scanner.nextInt(); satırı, girilen tamsayıyı adayinYasi değişkenine atar.
+        if (adayinYasi >= 0) {
+          break;
+        } else {
+          System.out.println("Yaş negatif olamaz. Lütfen geçerli bir yaş giriniz.");
+        }
+      } else {
+        System.out.println("Geçersiz giriş. Lütfen bir sayı giriniz.");
+        scanner.next(); // Geçersiz girişleri temizler
+      }
+    }
+
+    // Kullanıcıdan sağlık durumu bilgisi alınıyor
+    String saglikDurumuCevabi;
+
+    while (true) {
+      System.out.print("Adayın sağlık durumu uygun mu? (EVET/HAYIR): ");
+      saglikDurumuCevabi = scanner.next().trim().toUpperCase();
+      if (saglikDurumuCevabi.equals("EVET") || saglikDurumuCevabi.equals("HAYIR")) {
+        break;
+      } else {
+        System.out.println("Lütfen 'EVET' veya 'HAYIR' olarak cevap veriniz.");
+        scanner.nextLine(); // Hatalı girdiyi temizle
+      }
+    }
+    boolean adayinSaglikDurumu = saglikDurumuCevabi.equals("EVET"); // saglikDurumuCevabi değişkeninin değeri "EVET" ise adayinSaglikDurumu true, değilse false olur.
+
+    // Ehliyet Kontrolü
+    if (adayinSaglikDurumu) {
+      if (adayinYasi >= 18) {
+        System.out.println("Aday sağlıklı ve reşit. Ehliyet almaya uygundur.");
+      } else if (adayinYasi == 16) {
+        System.out.println("Aday sağlıklı ve stajyer olmaya uygundur.");
+      } else {
+        System.out.println("Adayın ehliyet almak için yaşı uygun değildir.");
       }
     } else {
-      System.out.println("Üzgünüm ehliyet için uygun değilisiniz");
+      System.out.println("Adayın ehliyet almak için sağlık durumu uygun değil.");
     }
+    scanner.close(); // Scanner nesnesi kapatılır. Bu, kaynakların doğru şekilde serbest bırakılmasını sağlar.
 
 
   }
